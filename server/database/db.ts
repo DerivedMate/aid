59d1d7cd793353c56e9c931fe90de79b867a573e
derivedmate <tosurowiec@gmail.com>
2021-04-08 00:19:40 +0200

    add ssl-rejection ignore to Pool

--- server/database/db.ts
+++ server/database/db.ts
@@ -1,7 +1,10 @@
 import pg = require('pg')
 
 const pool = new pg.Pool({
-  connectionString: process.env['DATABASE_URL']
+  connectionString: process.env['DATABASE_URL'],
+  ssl: {
+    rejectUnauthorized: false
+  }
 })
 
 export const query = <T>(q: string, params: string[]): Promise<pg.QueryResult<T>> => pool.query(q, params)
