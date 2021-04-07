24d4b0ed237c568fa7d0dd91858b56c446845898
derivedmate <tosurowiec@gmail.com>
2021-04-08 00:03:03 +0200

    rewrite postgres connection to connection_string

--- server/database/db.ts
+++ server/database/db.ts
@@ -1,6 +1,8 @@
 import pg = require('pg')
 
-const pool = new pg.Pool()
+const pool = new pg.Pool({
+  connectionString: process.env['DATABASE_URL']
+})
 
 export const query = <T>(q: string, params: string[]): Promise<pg.QueryResult<T>> => pool.query(q, params)
 
