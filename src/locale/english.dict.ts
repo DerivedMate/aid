1d529556e9b591ea2c6c736dbeff50d658efa33c
Tomasz Surowiec <xeloxpl@windowslive.com>
2021-04-09 13:17:54 +0200

    replace lorem ipsum

--- src/locale/english.dict.ts
+++ src/locale/english.dict.ts
@@ -19,8 +19,8 @@ const dict: Locale = {
   home: {
     toAbout: {
       title: 'Aid',
-      subtitle: 'Better. Faster. Safer.',
-      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, soluta.'
+      subtitle: 'Safety always near you',
+      body: 'Aid Mate to rewolocyjne rozwiązanie w zakresie monitorowania bezpieczeństwa i nadawania samodzielności osobom z niepełnosprawnościami, seniorom i innym grupom potrzebującym codziennego wsparcia.'
     }
   },
 
